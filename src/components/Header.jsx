3050ab0c20151362afcb6fe45033c1b07bb26b73
Gagi Shmagi <gagi.shmagi@gmail.com>
2021-08-25 13:49:36 +0300

    Added form to use react-router to search for page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,39 @@
 import logo from '../logo.svg';
 import "./Header.css";
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink, Link, withRouter } from 'react-router-dom';
 
-export default function Header() {
+function Header(props) {
+
+    let q = '',value = ''
+
+    const handleSearch = (e) => {
+      e.preventDefault()
+
+      q = e.target[0].name
+      value = e.target[0].value
+
+      props.history.push(`/contact?${q}=${value}`)
+
+    }
+
     return (
-        <div style={ {backgroundColor: "lightblue" }}>
+      <div style={ {backgroundColor: "lightblue" }}>
             <img  className="logo" src={logo} width="100"/>
             <nav>
             <ul>
               <li><NavLink exact activeStyle={{color:"red"}} style={{color:"blue"}} to="/">Home</NavLink></li>
               <li><NavLink activeStyle={{color:"green"}} style={{color:"blue"}} to="/contact">Contact Us</NavLink></li>
               <li><NavLink activeStyle={{color:"purple"}} style={{color:"blue"}} to="/about">About</NavLink></li>
-            </ul>  
+              <li><a href="https://www.google.com" >Google</a></li>
+            </ul>
+            <form onSubmit={handleSearch} action="/contact">
+                <input type="text" name="q"/>
+                <button>Search</button>
+            </form>
           </nav>
         </div>
     )
 }
+
+export default withRouter(Header)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
-import { BrowserRouter as Router, Link, NavLink, Redirect, Route, Switch } from "react-router-dom"
+import { BrowserRouter as Router, Link, NavLink, Redirect, Route, Switch, withRouter } from "react-router-dom"
 import Contact from "../components/Contact"
+import Header from "../components/Header"
 
 import HomePage from '../components/HomePage'
 import Page404 from "../components/Page404"
@@ -9,19 +10,19 @@ import Secret from "../components/Secret"
 const routes = function (){
     return (
         <Router>
-            <Link to={{
-                pathname:"/",
+            {/* <Link to={{
+                pathname:"/contact",
                 search:"?q=sadasdasda",
-            }}>Home</Link>
-            <Link to="/contact" >Contact Us</Link>
+            }}>Home</Link> */}
+            {/* <Link to="/contact" >Contact Us</Link>
             <NavLink exact style={{color:"blue"}} activeStyle={{color:"red"}} to="/">Home</NavLink>
             <NavLink style={{color:"blue"}} activeStyle={{color:"red"}} to="/home">Home</NavLink>
             <NavLink style={{color:"blue"}} activeStyle={{color:"red"}} to="/contact">Contact Us</NavLink>
             <NavLink style={{color:"blue"}} activeStyle={{color:"purple"}} to="/secret">Secret Page</NavLink>
 
-            <a href="https://www.google.com" >Google</a>
-
-
+            <a href="https://www.google.com" >Google</a> */}
+            <Header/>
+ 
             <Switch>
                 <Route exact path="/" component={HomePage}/>
                 <Route path="/home" render={(props) => (
